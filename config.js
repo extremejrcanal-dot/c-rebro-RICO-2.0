4d93a7d4b357099dfdbb50aaeb3f61278ba21b92
extremejrcanal-dot <extremejrcanal@gmail.com>
2026-07-29 12:54:29 -0300

    Update config.js

--- config.js
+++ config.js
@@ -20,8 +20,7 @@
 
 window.CEREBRO_RICO = {
 
-  SUPABASE_URL: "https://abcdefghijk.supabase.co",
-
+  SUPABASE_URL: "https://mtpyrperjnecjephrtbc.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10cHlycGVyam5lY2plcGhydGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNTU5MDUsImV4cCI6MjA5MzkzMTkwNX0.UhIOk1sVLnbCi4vpwROWFzSrJ_753brEIwOqGkRJHfA"
 
 };
